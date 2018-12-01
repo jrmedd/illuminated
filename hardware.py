a130784f3bc452a3527427c48aa0d977c6813dfa
jrmedd <hello@jamesmedd.co.uk>
2018-12-01 12:56:09 +0000

    Better rhythm grabbing

--- hardware.py
+++ hardware.py
@@ -5,7 +5,7 @@ import serial
 import os
 
 RHYTHM_URL = "https://illumin.im/rhythms"
-RHYTHM_URL = "http://127.0.0.1:5000/rhythms"
+#RHYTHM_URL = "http://127.0.0.1:5000/rhythms"
 
 #TEENSY = serial.Serial(port='/dev/tty.usbmodem1411', baudrate=115200)
 
@@ -20,9 +20,9 @@ while True:
         print("Unable to access")
     if oldest_rhythm:
         beats = oldest_rhythm.get('_taps')
-        #comma-separate the beats as ints in a string, the 'BEATS' bit gets chopped off when first reading on the Arduino
-        #beats_string = "BEATS,%s\n" % (','.join([str(int(beat)) for beat in beats])) 
-        #TEENSY.write(beats_string.encode())
+        comma-separate the beats as ints in a string, the 'BEATS' bit gets chopped off when first reading on the Arduino
+        beats_string = "BEATS,%s\n" % (','.join([str(int(beat)) for beat in beats])) 
+        TEENSY.write(beats_string.encode())
         update = requests.post(RHYTHM_URL, headers=API_HEADER ,data={'_id':oldest_rhythm.get('_id'), 'illuminated':True})
         time.sleep((oldest_rhythm.get('duration')/1000)*9)
     else:
