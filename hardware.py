07b4bcf1e8247dd62c9cb81ffbe10d6f91cadc55
jrmedd <hello@jamesmedd.co.uk>
2018-12-02 14:47:35 +0000

    Fix hardware to ensure not too many beats

--- hardware.py
+++ hardware.py
@@ -7,7 +7,7 @@ import os
 RHYTHM_URL = "https://illumin.im/rhythms"
 #RHYTHM_URL = "http://127.0.0.1:5000/rhythms"
 
-TEENSY = serial.Serial(port='/dev/tty.usbmodem1411', baudrate=115200)
+TEENSY = serial.Serial(port='/dev/ttyACM0', baudrate=115200)
 
 API_KEY = os.environ.get('API_KEY')
 
@@ -19,8 +19,8 @@ while True:
     except:
         print("Unable to access")
     if oldest_rhythm:
-        beats = oldest_rhythm.get('_taps')
-        comma-separate the beats as ints in a string, the 'BEATS' bit gets chopped off when first reading on the Arduino
+        beats = oldest_rhythm.get('_taps')[0:19] #always strip it to 20 beats
+        #comma-separate the beats as ints in a string, the 'BEATS' bit gets chopped off when first reading on the Arduino
         beats_string = "BEATS,%s\n" % (','.join([str(int(beat)) for beat in beats])) 
         TEENSY.write(beats_string.encode())
         update = requests.post(RHYTHM_URL, headers=API_HEADER ,data={'_id':oldest_rhythm.get('_id'), 'illuminated':True})
